3c3789cf181b4418cc47f1eab4b5517998d5e972
kun <kunhello@outlook.com>
2017-06-19 16:13:57 +0800

    add search nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Breadcrumb from '../breadcrumb/breadcrumb';
 import AccountInfo from '../account-info/account-info';
+import NavigationSearch from '../navigation-search/navigation-search';
 import styles from './header.styl';
 
 const Header = ({ toggle }) => (
   <header className={ toggle ? `${styles.container} ${styles.toggle}`: styles.container} >
     <Breadcrumb />
     <AccountInfo />
+    <NavigationSearch />
   </header>
 );
 
